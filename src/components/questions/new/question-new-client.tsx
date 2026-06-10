c99e464171a352b6b429db472dc0f9cc92306ffc
roryTheGnome <s29991@pjwstk.edu.pl>
2026-06-10 13:18:35 +0200

    fix: navigate on create without editor onSuccess callback

--- src/components/questions/new/question-new-client.tsx
+++ src/components/questions/new/question-new-client.tsx
@@ -2,25 +2,20 @@
 
 import { useTranslations } from 'next-intl'
 
-import {
-  QuestionEditor,
-  type QuestionSubmitCallbacks,
-} from '@/components/questions/editor/question-editor'
+import { QuestionEditor } from '@/components/questions/editor/question-editor'
 import { useCreateQuestion } from '@/components/questions/use-question-mutations'
 import { useRouter } from '@/i18n/navigation'
 import { type QuestionInput } from '@/lib/api'
-import { questionToEditorInput } from '@/lib/question-editor/parsers'
 
 export function QuestionNewClient() {
   const t = useTranslations('questions.newPage')
   const router = useRouter()
   const { mutate: createQuestion, isPending: submitting } = useCreateQuestion()
 
-  function handleSubmit(value: QuestionInput, { onSuccess }: QuestionSubmitCallbacks) {
+  function handleSubmit(value: QuestionInput) {
     createQuestion(value, {
       onSuccess: (question) => {
         router.push(`/questions/${question.id}`)
-        onSuccess(questionToEditorInput(question))
       },
     })
   }
