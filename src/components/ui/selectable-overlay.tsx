2ba320c2cd829d5b553b1555012d6d8be060d1cc
nott-po <nesterovapolina55@gmail.com>
2026-07-02 15:33:56 +0200

    (refactor): add picked state to card and interactive option to selectable overlay

--- src/components/ui/selectable-overlay.tsx
+++ src/components/ui/selectable-overlay.tsx
@@ -5,16 +5,21 @@ import { cn } from '@/lib/utils'
 interface SelectableOverlayProps extends HTMLAttributes<HTMLDivElement> {
   marker: ReactNode
   children: ReactNode
+  interactive?: boolean
 }
 
 export function SelectableOverlay({
   marker,
   children,
   className,
+  interactive = false,
   ...props
 }: SelectableOverlayProps) {
   return (
-    <div className={cn('group relative', className)} {...props}>
+    <div
+      className={cn('group relative', interactive && 'cursor-pointer', className)}
+      {...props}
+    >
       <span
         className="absolute right-4 top-4 z-10 transition-transform duration-200 group-hover:-translate-y-1"
         onClick={(e) => {
